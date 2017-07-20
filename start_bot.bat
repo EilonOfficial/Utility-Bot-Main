16935df028f49dcad7670b88978ee6fa162f52df
Rawr <gamingbros975@gmail.com>
2017-07-19 21:29:07 -0400

    gfj

--- start_bot.bat
+++ start_bot.bat
@@ -1,3 +1,3 @@
 @echo off
-node --harmony C:\Users\NewPoof\Desktop\ModerationBot-master\node_modules\nodemon\bin\nodemon.js
+node --harmony C:\Users\NewPoof\Desktop\Utility-Bot\node_modules\nodemon\bin\nodemon.js
 PAUSE
